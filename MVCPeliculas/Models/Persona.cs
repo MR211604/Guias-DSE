a3c68b8100bbf53a43e4204a2f606a6355c42d3c
MR211604 <elmastrollxd34@gmail.com>
2026-07-09 21:22:13 -0600

    chore: agregando ejercicio 2

---
+++ MVCPeliculas/Models/Persona.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCPeliculas.Models;
+
+public class Persona
+{
+    public string DUI { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+
+    public string Direccion { get; set; }
+    public EmailAddressAttribute Correo { get; set; }
+}

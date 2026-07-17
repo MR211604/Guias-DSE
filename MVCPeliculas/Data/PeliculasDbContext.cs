b61e2e15bad3786a572434375c5416478b0f59c4
MR211604 <elmastrollxd34@gmail.com>
2026-07-16 20:06:40 -0600

    chore: agregando ejercicio 1

--- MVCPeliculas/Data/PeliculasDbContext.cs
+++ MVCPeliculas/Data/PeliculasDbContext.cs
@@ -9,4 +9,5 @@ public class PeliculasDbContext : DbContext
     {
     }
     public DbSet<Pelicula> Peliculas { get; set; }
+    public DbSet<Genero> Generos { get; set; }
 }
--- MVCPeliculas/Models/Pelicula.cs
+++ MVCPeliculas/Models/Pelicula.cs
@@ -1,10 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MVCPeliculas.Models;
 
 public class Pelicula
 {
     public int Id { get; set; }
+    [StringLength(250)]
+    [Required]
     public string Titulo { get; set; }
+    
     public DateTime FechaLanzamiento { get; set; }
-    public string Genero { get; set; }
+  
+    [Column(TypeName ="money")]
+    [Required]
     public decimal Precio { get; set; }
+
+    [StringLength(15)]
+    [Required]
+    public string Director { get; set; }
+    [Required]
+    [ForeignKey("GeneroPelicula")]
+    public int GeneroId { get; set; }
+    public Genero? GeneroPelicula { get; set; }
 }

6ed2abf57b5c6e46a92ec374dffbd095595ef76c
MR211604 <elmastrollxd34@gmail.com>
2026-07-23 18:06:37 -0600

    Agregando contenido de guia 04

---
+++ MVCPeliculas/Migrations/20260723231456_Agregando registros.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCPeliculas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandoregistros : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

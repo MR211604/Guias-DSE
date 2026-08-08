6b84621384d5483e2e3e7411fc7e37925d1d4be5
MR211604 <elmastrollxd34@gmail.com>
2026-08-07 23:06:09 -0600

    Configuracion para segunda parte de guia

---
+++ DockerMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DockerMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerMvc.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+DbContext(options)
+    {
+        public DbSet<Persona> Personas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}

ec3f8f97beafec57d2904919409210eeaf49c283
MR211604 <elmastrollxd34@gmail.com>
2026-08-27 17:53:34 -0600

    Agregando autenticacion general a las API de Libros y Productos

---
+++ LibrosAPI/Program.cs
@@ -0,0 +1,33 @@
+using JwtAuthenticationManager;
+using LibrosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<LibrosDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCustomJwtAuthentication();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
